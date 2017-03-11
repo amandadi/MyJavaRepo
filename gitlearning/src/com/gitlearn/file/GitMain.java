79444408f9e4b8d5e78d10a52b53e14a7951dd4d
ramya <ramya@192.168.0.10>
2017-03-11 14:12:03 -0600

    Learning to merge from one local branch to other

--- gitlearning/src/com/gitlearn/file/GitMain.java
+++ gitlearning/src/com/gitlearn/file/GitMain.java
@@ -5,6 +5,7 @@ public class GitMain {
 	public static void main(String[] args) {
 		System.out.println("My Git Learning!!!");
 		System.out.println("Commited my first change");
+		System.out.println("Learn to merge from master to release");
 	}
 
 }

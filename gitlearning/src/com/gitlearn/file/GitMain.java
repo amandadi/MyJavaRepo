b2d10e6c8af2aede9a12b520e163e9f0c29bebe4
ramya <ramya@192.168.0.10>
2017-03-11 15:51:57 -0600

    check-in to remote

--- gitlearning/src/com/gitlearn/file/GitMain.java
+++ gitlearning/src/com/gitlearn/file/GitMain.java
@@ -5,6 +5,7 @@ public class GitMain {
 	public static void main(String[] args) {
 		System.out.println("My Git Learning!!!");
 		System.out.println("Commited my first change");
+		System.out.println("Check-in this change to remote");
 	}
 
 }

db5e2b63a6bacad3c6c3f02e4e001d2b0826a5cb
ramya <ramya@192.168.0.10>
2017-03-11 17:44:09 -0600

    Committing change from local to remote

--- gitlearning/src/com/gitlearn/file/GitMain.java
+++ gitlearning/src/com/gitlearn/file/GitMain.java
@@ -5,7 +5,7 @@ public class GitMain {
 	public static void main(String[] args) {
 		System.out.println("My Git Learning!!!");
 		System.out.println("Commited my first change");
-		System.out.println("Check-in this change to remote");
+		System.out.println("Able to clone this project from remote");
 	}
 
 }
